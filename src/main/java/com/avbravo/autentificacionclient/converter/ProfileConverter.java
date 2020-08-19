f0ce1609cd0e055b54f2e926fb64f86de36d3366
avbravo <avbravo@avbravo>
2020-08-19 12:15:02 -0500

    login converter

--- src/main/java/com/avbravo/autentificacionclient/converter/ProfileConverter.java
+++ src/main/java/com/avbravo/autentificacionclient/converter/ProfileConverter.java
@@ -6,7 +6,9 @@
 package com.avbravo.autentificacionclient.converter;
 
 import com.avbravo.autentificacionclient.entity.Profile;
+import com.avbravo.autentificacionclient.entity.User;
 import com.avbravo.autentificacionclient.services.ProfileServices;
+import com.avbravo.jmoordb.configuration.JmoordbContext;
 import com.avbravo.jmoordb.mongodb.history.services.ErrorInfoServices;
 import com.avbravo.jmoordb.util.JmoordbUtil;
 import java.util.Optional;
@@ -25,25 +27,44 @@ import javax.inject.Named;
 @RequestScoped
 public class ProfileConverter implements Converter {
 
-    @Inject
-    ErrorInfoServices errorServices;
-    @Inject
-    ProfileServices profileServices;
-
-    @Override
+   @Override
     public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String s) {
         Profile profile = new Profile();
+
         try {
-            if (!s.equals("null")) {
-              profile.setIdprofile(Integer.parseInt(s));
-//                Optional<Profile> optional = profileServices.findByIdprofile(Integer.parseInt(s));
-//                if (optional.isPresent()) {
-//                    profile = optional.get();
-//                }
+            System.out.println("---------------Analizando converter.............");
+            if (s == null || s.equals("null") || s.equals("")) {
+                profile.setIdprofile(0);
+                System.out.println(">>>>>>>>>>>>><<< Converte es 0");
+            } else {
+                System.out.println(">>>>>>>>>>>>><<<< voy a convertirlo a entero " + s);
+                Integer idprofile = Integer.parseInt(s);
+                /*                
+Recuperael user dl context
+  Busca en el embebido    
+                
+                 */
+                System.out.println(">>>>>>>>><voy a obtener el user del contecto");
+                if ((User) JmoordbContext.get("jmoordb_user") == null) {
+                    System.out.println(" >>>> user nop esta en el contexto");
+                } else {
+                    System.out.println(">>>>>>>>>>><< lo encontro en el contexto voy a buscar");
+                    User user = (User) JmoordbContext.get("jmoordb_user");
+
+                    System.out.println(">>>>> procedo con el for");
+                    for (Profile p : user.getProfile()) {
+                        System.out.println("Comparando " + p.getIdrole() + "  con  " + idprofile);
+                        if (p.getIdrole().equals(idprofile)) {
+                            System.out.println("Encontro en el embebido");
+                            profile = p;
+                        }
+                    }
+                }
+
             }
         } catch (Exception e) {
-//            errorServices.errorMessage(JmoordbUtil.nameOfClass(), JmoordbUtil.nameOfMethod(), e.getLocalizedMessage(), e);
-          JmoordbUtil.errorDialog( JmoordbUtil.nameOfMethod(),e.getLocalizedMessage());
+
+            JmoordbUtil.errorDialog(JmoordbUtil.nameOfMethod(), e.getLocalizedMessage());
         }
         return profile;
     }
@@ -52,16 +73,22 @@ public class ProfileConverter implements Converter {
     public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object o) {
         String r = "";
         try {
+            System.out.println("@@@@@@@@@@@@@@ llego a getAsSDtring");
             if (o instanceof Profile) {
+                System.out.println("@@@@@@@ es un profile");
                 Profile profile = (Profile) o;
+                System.out.println("@@@@@ instanciare");
                 r = String.valueOf(profile.getIdprofile());
+                System.out.println("@@@@@ ya lo convertire");
             } else if (o instanceof String) {
+                System.out.println("@@@@@@@ es un String");
                 r = (String) o;
             }
         } catch (Exception e) {
-             JmoordbUtil.errorDialog( JmoordbUtil.nameOfMethod(),e.getLocalizedMessage());
 //            errorServices.errorMessage(JmoordbUtil.nameOfClass(), JmoordbUtil.nameOfMethod(), e.getLocalizedMessage(), e);
+            JmoordbUtil.errorDialog(JmoordbUtil.nameOfMethod(), e.getLocalizedMessage());
         }
+        System.out.println("@@@@@@@@@ retorna "+r);
         return r;
     }
 
