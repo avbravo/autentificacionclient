1d34971b6149da4fc1a93d0c36b6513f5051935b
avbravo <avbravo@gmail.com>
2021-05-26 13:06:50 -0500

    ajustes en entidad access y repository para guardar los accesos de los usuarios

--- src/main/java/com/avbravo/autentificacionclient/entity/Access.java
+++ src/main/java/com/avbravo/autentificacionclient/entity/Access.java
@@ -21,6 +21,8 @@ public class Access {
      @Id
     private Integer idaccess;
     private String username;
+    private String event;
+    private String ip;
     private Date date;
     private Integer idprofile;
     private Integer idapplicative;
@@ -30,6 +32,43 @@ public class Access {
     public Access() {
     }
 
+    public Access(Integer idaccess, String username, String event, String ip, Date date, Integer idprofile, Integer idapplicative, Integer idrole, Integer iddepartament) {
+        this.idaccess = idaccess;
+        this.username = username;
+        this.event = event;
+        this.ip = ip;
+        this.date = date;
+        this.idprofile = idprofile;
+        this.idapplicative = idapplicative;
+        this.idrole = idrole;
+        this.iddepartament = iddepartament;
+    }
+
+    
+    
+    
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+  
+    
+    
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    
+    
+    
     public String getUsername() {
         return username;
     }
@@ -110,4 +149,65 @@ public class Access {
         }
         return EqualsBuilder.reflectionEquals(this, object);
     }
+
+    
+      public static class Builder {
+
+     private Integer idaccess;
+    private String username;
+    private String event;
+    private String ip;
+    private Date date;
+    private Integer idprofile;
+    private Integer idapplicative;
+    private Integer idrole;
+    private Integer iddepartament;
+
+         public Builder idaccess(Integer idaccess) {
+            this.idaccess = idaccess;
+            return this;
+        }
+         public Builder username(String username) {
+            this.username = username;
+            return this;
+        }
+         public Builder ip(String ip) {
+            this.ip = ip;
+            return this;
+        }
+         public Builder date(Date date) {
+            this.date = date;
+            return this;
+        }
+         public Builder event(String event) {
+            this.event= event;
+            return this;
+        }
+         public Builder idprofile(Integer idprofile) {
+            this.idprofile= idprofile;
+            return this;
+        }
+         public Builder idapplicative(Integer idapplicative) {
+            this.idapplicative= idapplicative;
+            return this;
+        }
+         public Builder idrole(Integer idrole) {
+            this.idrole= idrole;
+            return this;
+        }
+         public Builder iddepartament(Integer iddepartament) {
+            this.iddepartament= iddepartament;
+            return this;
+        }
+       
+      
+          public Access build() {
+            return new Access(idaccess, username, event, ip, date, idprofile, idapplicative, idrole, iddepartament);
+
+        }
+         
+         
+    }
 }
+
+         
