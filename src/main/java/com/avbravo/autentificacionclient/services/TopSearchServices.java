e9ca74026048b3b2885245267c9990ccf8f9cd6c
avbravo <avbravo@avbravo>
2021-07-08 15:09:55 -0500

    topsearch actualizado

---
+++ src/main/java/com/avbravo/autentificacionclient/services/TopSearchServices.java
@@ -0,0 +1,334 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.autentificacionclient.services;
+
+import com.avbravo.autentificacionclient.entity.TopSearch;
+import com.avbravo.autentificacionclient.producer.AuthentificationProducer;
+import com.avbravo.autentificacionclient.producer.MicroservicesProducer;
+import com.avbravo.jmoordb.util.JmoordbDocument;
+import com.avbravo.jmoordb.util.JmoordbUtil;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author avbravo
+ */
+@Stateless
+public class TopSearchServices implements Serializable {
+
+    String directoryLogger = JmoordbUtil.isLinux() ? JmoordbUtil.userHome() + JmoordbUtil.fileSeparator() + "autentificacionclient" + JmoordbUtil.fileSeparator() + "logs" + JmoordbUtil.fileSeparator() + "logger.json" : "C:\\autentificacionclient\\logs\\logger.json";
+    private static final String PASS = "pass";
+    private static final String FAIL = "fail";
+    private static final String SUCCESS_RESULT = "<result>success</result>";
+  Exception exception;
+    @Inject
+    MicroservicesProducer microservicesProducer;
+
+    @Inject
+    AuthentificationProducer authentificationProducer;
+    
+     // <editor-fold defaultstate="collapsed" desc=" set/get)">
+    
+    
+    public Exception getException() {
+        return exception;
+    }
+
+    public void setException(Exception exception) {
+        this.exception = exception;
+    }
+    
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="List<TopSearch> findAll()">
+    public List<TopSearch> findAll() {
+        List<TopSearch> topsearchList = new ArrayList<>();
+        try {
+
+            Client client = ClientBuilder.newClient();
+            client.register(authentificationProducer.httpAuthenticationFeature());
+            WebTarget target = client.target(microservicesProducer.microservicesHost() + "/autentificacion/resources/topsearch/findall");
+
+            GenericType<List<TopSearch>> data = new GenericType<List<TopSearch>>() {
+            };
+
+            topsearchList = target.request(MediaType.APPLICATION_JSON).get(data);
+
+        } catch (Exception e) {
+            exception = new Exception(JmoordbUtil.nameOfMethod() + " " + e.getLocalizedMessage());
+            JmoordbUtil.appendTextToLogErrorFile(this.directoryLogger, JmoordbUtil.nameOfClass(), JmoordbUtil.nameOfMethod(), e.getLocalizedMessage(), e);
+            System.out.println("findAll()" + e.getLocalizedMessage());
+        }
+        return topsearchList;
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Boolean add(TopSearch topsearch)">
+    public Boolean add(TopSearch topsearch) {
+        try {
+            Client client = ClientBuilder.newClient();
+            client.register(authentificationProducer.httpAuthenticationFeature());
+            WebTarget webTarget
+                    = client.target(microservicesProducer.microservicesHost() + "/autentificacion/resources/topsearch/add");
+
+            Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+            Response response = invocationBuilder.post(Entity.entity(topsearch, MediaType.APPLICATION_JSON));
+
+          //  System.out.println(response.getStatus());
+            if (response.getStatus() == 400) {
+                 exception = new Exception(response.readEntity(String.class));
+                return false;
+            }
+//            System.out.println(response.readEntity(String.class
+//            ));
+            return true;
+        } catch (Exception e) {
+            exception = new Exception(JmoordbUtil.nameOfMethod() + " " + e.getLocalizedMessage());
+            JmoordbUtil.appendTextToLogErrorFile(this.directoryLogger, JmoordbUtil.nameOfClass(), JmoordbUtil.nameOfMethod(), e.getLocalizedMessage(), e);
+            System.out.println("errort" + e.getLocalizedMessage());
+        }
+        return false;
+    }
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="Boolean update(TopSearch topsearch)">
+    public Boolean update(TopSearch topsearch) {
+        try {
+            Client client = ClientBuilder.newClient();
+            client.register(authentificationProducer.httpAuthenticationFeature());
+            WebTarget webTarget
+                    = client.target(microservicesProducer.microservicesHost() + "/autentificacion/resources/topsearch/update");
+
+            Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+            Response response = invocationBuilder.put(Entity.entity(topsearch, MediaType.APPLICATION_JSON));
+
+            System.out.println(response.getStatus());
+            if (response.getStatus() == 400) {
+                 exception = new Exception(response.readEntity(String.class));
+                return false;
+            }
+//            System.out.println(response.readEntity(String.class
+//            ));
+            return true;
+        } catch (Exception e) {
+            exception = new Exception(JmoordbUtil.nameOfMethod() + " " + e.getLocalizedMessage());
+            JmoordbUtil.appendTextToLogErrorFile(this.directoryLogger, JmoordbUtil.nameOfClass(), JmoordbUtil.nameOfMethod(), e.getLocalizedMessage(), e);
+            System.out.println("errort" + e.getLocalizedMessage());
+        }
+        return false;
+    }
+// </editor-fold>
+// <editor-fold defaultstate="collapsed" desc="Boolean delete(TopSearch topsearch)">
+
+    public Boolean delete(TopSearch topsearch) {
+        try {
+            Client client = ClientBuilder.newClient();
+            client.register(authentificationProducer.httpAuthenticationFeature());
+            WebTarget webTarget
+                    = client.target(microservicesProducer.microservicesHost() + "/autentificacion/resources/topsearch/delete");
+
+            Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+            Response response = invocationBuilder.post(Entity.entity(topsearch, MediaType.APPLICATION_JSON));
+
+            System.out.println(response.getStatus());
+            if (response.getStatus() == 400) {
+                 exception = new Exception(response.readEntity(String.class));
+                return false;
+            }
+            System.out.println(response.readEntity(String.class
+            ));
+            return true;
+        } catch (Exception e) {
+            exception = new Exception(JmoordbUtil.nameOfMethod() + " " + e.getLocalizedMessage());
+            JmoordbUtil.appendTextToLogErrorFile(this.directoryLogger, JmoordbUtil.nameOfClass(), JmoordbUtil.nameOfMethod(), e.getLocalizedMessage(), e);
+            System.out.println("errort" + e.getLocalizedMessage());
+        }
+        return false;
+    }
+// </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="ptional<TopSearch> findByTopSearch(Integer idtopsearch) ">
+    /**
+     * consulta por codigo_pedido impresa
+     *
+     * @param codigo_
+     * @return
+     */
+    public Optional<TopSearch> findByIdtopsearch(Integer idtopsearch) {
+        TopSearch topsearch = new TopSearch();
+        try {
+         
+            Client client = ClientBuilder.newClient();
+            client.register(authentificationProducer.httpAuthenticationFeature());
+            topsearch = client
+                    .target(microservicesProducer.microservicesHost() + "/autentificacion/resources/topsearch/search/")
+                    .path("/{idtopsearch}")
+                    .resolveTemplate("idtopsearch", idtopsearch)
+                    .request(MediaType.APPLICATION_JSON)
+                    .get(TopSearch.class
+                    );
+            
+            if(topsearch == null  || topsearch.getIdtopsearch() == null || topsearch.getKey() ==null){
+           
+                 return Optional.empty();
+            }
+           
+            return Optional.of(topsearch);
+            //String result = FAIL;
+        } catch (Exception e) {
+            exception = new Exception(JmoordbUtil.nameOfMethod() + " " + e.getLocalizedMessage());
+            JmoordbUtil.appendTextToLogErrorFile(this.directoryLogger, JmoordbUtil.nameOfClass(), JmoordbUtil.nameOfMethod(), e.getLocalizedMessage(), e);
+            System.out.println("findByIdtopsearch() " + e.getLocalizedMessage());
+        }
+        return Optional.empty();
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="List<TopSearch> complete( String query)">
+    public List<TopSearch> complete(String query) {
+        List<TopSearch> suggestions = new ArrayList<>();
+        try {
+
+            if (query == null || query.isEmpty()) {
+                query = "{{complete}}";
+            }
+            Client client = ClientBuilder.newClient();
+            client.register(authentificationProducer.httpAuthenticationFeature());
+            suggestions = client
+                    .target(microservicesProducer.microservicesHost() + "/autentificacion/resources/topsearch/autocomplete/")
+                    .path("/{query}")
+                    .resolveTemplate("query", query)
+                    .request(MediaType.APPLICATION_JSON)
+                    .get(new GenericType<List<TopSearch>>() {
+                    });
+
+        } catch (Exception e) {
+            exception = new Exception(JmoordbUtil.nameOfMethod() + " " + e.getLocalizedMessage());
+            JmoordbUtil.appendTextToLogErrorFile(this.directoryLogger, JmoordbUtil.nameOfClass(), JmoordbUtil.nameOfMethod(), e.getLocalizedMessage(), e);
+            System.out.println("complete() " + e.getLocalizedMessage());
+            JmoordbUtil.errorDialog("complete()", e.getLocalizedMessage());
+        }
+
+        return suggestions;
+    }
+    // </editor-fold>
+
+        
+    // <editor-fold defaultstate="collapsed" desc=" List<TopSearch> jsonQuery(@QueryParam("query") String query , @QueryParam("sort") String sort, @QueryParam("pagenumber") Integer pageNumber, @QueryParam("rowforpage") Integer rowForPage )">
+
+  public  List<TopSearch> jsonQuery( String query ,  String sort,
+     Integer pageNumber,  Integer rowForPage ){
+        List<TopSearch> suggestions = new ArrayList<>();
+        try {
+ 
+
+            Client client = ClientBuilder.newClient();
+            client.register(authentificationProducer.httpAuthenticationFeature());
+            suggestions = client
+                    .target(microservicesProducer.microservicesHost() + "/autentificacion/resources/topsearch/jsonquery/")                    
+                    .queryParam("query", JmoordbDocument.encodeJson(query))
+                    .queryParam("sort",JmoordbDocument.encodeJson(sort))
+                    .queryParam("pagenumber", pageNumber)
+                    .queryParam("rowforpage", rowForPage)
+                    .request(MediaType.APPLICATION_JSON)
+                    .get(new GenericType<List<TopSearch>>() {
+                    });
+
+        } catch (Exception e) {
+            exception = new Exception(JmoordbUtil.nameOfMethod() + " " + e.getLocalizedMessage());
+            JmoordbUtil.appendTextToLogErrorFile(this.directoryLogger, JmoordbUtil.nameOfClass(), JmoordbUtil.nameOfMethod(), e.getLocalizedMessage(), e);
+            System.out.println(JmoordbUtil.nameOfMethod()+ e.getLocalizedMessage());
+            JmoordbUtil.errorDialog(JmoordbUtil.nameOfMethod(), e.getLocalizedMessage());
+        }
+
+        return suggestions;
+    }
+    // </editor-fold>   
+  
+  // <editor-fold defaultstate="collapsed" desc=" List<TopSearch> jsonQueryWithoutPagination( @QueryParam("query") String query , @QueryParam("sort") String sort  ){">
+   
+   public List<TopSearch> jsonQueryWithoutPagination( String query ,  String sort  ){
+        List<TopSearch> suggestions = new ArrayList<>();
+        try {
+
+            Client client = ClientBuilder.newClient();
+            client.register(authentificationProducer.httpAuthenticationFeature());
+            suggestions = client
+                    .target(microservicesProducer.microservicesHost() + "/autentificacion/resources/topsearch/jsonquerywithoutpagination/")                    
+                 .queryParam("query", JmoordbDocument.encodeJson(query))
+                    .queryParam("sort",JmoordbDocument.encodeJson(sort))
+                  
+                    .request(MediaType.APPLICATION_JSON)
+                    .get(new GenericType<List<TopSearch>>() {
+                    });
+        } catch (Exception e) {
+            exception = new Exception(JmoordbUtil.nameOfMethod() + " " + e.getLocalizedMessage());
+            JmoordbUtil.appendTextToLogErrorFile(this.directoryLogger, JmoordbUtil.nameOfClass(), JmoordbUtil.nameOfMethod(), e.getLocalizedMessage(), e);
+            System.out.println("lisfOfPage() " + e.getLocalizedMessage());
+            JmoordbUtil.errorDialog("lisfOfPage()", e.getLocalizedMessage());
+        }
+
+        return suggestions;
+    }
+    // </editor-fold>   
+    
+    
+   
+    // <editor-fold defaultstate="collapsed" desc="Integer countJsonQuery(String query)">
+
+    /**
+     * devuelve el contador de documentos en base a un json query
+     * @param query
+     * @return 
+     */
+
+    public Integer countJsonQuery(String query) {
+        Integer total = 0;
+        try {
+
+            Client client = ClientBuilder.newClient();
+            client.register(authentificationProducer.httpAuthenticationFeature());
+
+            WebTarget webTarget
+                    = client.target(microservicesProducer.microservicesHost() + "/autentificacion/resources/topsearch/countjsonquery")
+                             .queryParam("query", JmoordbDocument.encodeJson(query));
+
+            Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+            Response response = invocationBuilder.get();
+            if (response.getStatus() == 201) {
+                total = Integer.parseInt(response.readEntity(String.class));
+
+            }
+
+            if (response.getStatus() == 400) {
+                exception = new Exception(response.readEntity(String.class));
+                return 0;
+            }
+
+        } catch (Exception e) {
+            exception = new Exception(JmoordbUtil.nameOfMethod() + " " + e.getLocalizedMessage());
+            JmoordbUtil.appendTextToLogErrorFile(this.directoryLogger, JmoordbUtil.nameOfClass(), JmoordbUtil.nameOfMethod(), e.getLocalizedMessage(), e);
+            System.out.println(JmoordbUtil.nameOfMethod() + " " + e.getLocalizedMessage());
+            JmoordbUtil.errorDialog(JmoordbUtil.nameOfMethod(), e.getLocalizedMessage());
+        }
+
+        return total;
+    }
+    // </editor-fold>
+}
