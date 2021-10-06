c8d48f5d134d96cb819951be7443ebf2215a35ec
avbravo <avbravo@avbravo>
2021-10-06 14:54:58 -0500

    con entidades Building y TipoArea

---
+++ src/main/java/com/avbravo/limpieza/converter/TipoAreaConverter.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.limpieza.converter;
+
+import com.avbravo.limpieza.entity.TipoArea;
+import com.avbravo.limpieza.services.TipoAreaServices;
+import java.util.Optional;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author avbravo
+ */
+@Named
+@RequestScoped
+public class TipoAreaConverter implements Converter {
+    @Inject
+    TipoAreaServices tipoAreagServices;
+  @Override
+    public String getAsString(FacesContext context, UIComponent component, Object modelValue) {
+        if (modelValue == null) {
+            return "";
+        }
+
+        if (modelValue instanceof TipoArea) {
+          return String.valueOf(((TipoArea) modelValue).getIdtipoarea());
+        } else {
+            throw new ConverterException(new FacesMessage(modelValue + " is not a valid from Converter"));
+        }
+    }
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String submittedValue) {
+        TipoArea a = new TipoArea();
+        if (tipoAreagServices == null) {
+            System.out.println("Service is nich");
+        }
+
+        if (submittedValue == null || submittedValue.isEmpty()) {
+            System.out.println("submitted = nil");
+            return null;
+        }
+
+        try {
+            Optional<TipoArea> optional = tipoAreagServices.findByIdTipoarea(Integer.parseInt(submittedValue));
+            if (optional.isPresent()) {
+                a = optional.get();
+            }
+            return a;
+        } catch (NumberFormatException e) {
+            throw new ConverterException(new FacesMessage(submittedValue + " is not a valid selecction from Converter"), e);
+        }
+    }
+
+}
